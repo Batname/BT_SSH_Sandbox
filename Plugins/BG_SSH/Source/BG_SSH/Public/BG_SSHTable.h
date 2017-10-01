5d0055b9da6955c03332fcb2f548897982fa0660
Batname <dadubinin@gmail.com>
2017-10-01 12:39:20 +0200

    add parent class

---
+++ Plugins/BG_SSH/Source/BG_SSH/Public/BG_SSHTable.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BG_SSHActor.h"
+#include "BG_SSHTable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BG_SSH_API ABG_SSHTable : public ABG_SSHActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
